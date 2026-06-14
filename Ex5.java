76aed8ef0bdd6ec1dabb6e7b03e2c51d03700712
WjCyber <wesjds.cop@gmail.com>
2026-06-14 03:01:46 -0300

    Exercise 5, completed

---
+++ Ex5.java
@@ -0,0 +1,18 @@
+/*Crie uma tabuada de 1 a 10 usando for. */
+
+
+public class Ex5 {
+    public static void main(String[]args) {
+
+        for(int i = 1; i<=10; i++) {
+            System.out.println("================");
+            System.out.println("Tabuada do " + i);
+            System.out.println("================");
+            
+            for(int j = 1; j<=10; j++){
+                System.out.println(i + " x " + j +" = " + i*j);
+                System.out.println("--------------");
+            }
+        }
+    }
+}
